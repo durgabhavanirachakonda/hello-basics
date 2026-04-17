857ea6a7abd052c99b66484e224c4c2099c8c134
durgabhavanirachakonda <durgabhavanirachakonda6@gmail.com>
2026-04-17 22:27:33 +0530

    Create weird.py

---
+++ weird.py
@@ -0,0 +1,9 @@
+num = int(input())
+if num % 2 == 1:
+    print("Weird")
+elif num % 2 == 0 and 2 <= num <= 5:
+    print("Not Weird")
+elif num % 2 == 0 and 6 <= num <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
